6d8cc79d47c0288f3e38640c6d0775c2b0ffd53d
nslupysz <nslupysz@gmail.com>
2021-03-02 04:07:42 +1300

    pug

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import { localsMiddleware } from "./middleware";
+import routes from "./routes";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+
+const app = express();
+
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+app.set("view engine", "pug");
+app.use(localsMiddleware);
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;
